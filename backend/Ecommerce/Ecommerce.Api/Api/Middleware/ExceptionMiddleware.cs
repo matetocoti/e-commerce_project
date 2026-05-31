98045ad346dceedca08c68810b158603607d3aa6
Matheus Costa <92372784+matetocoti@users.noreply.github.com>
2026-05-30 21:33:19 -0300

    refactor(backend): simplify ExceptionMiddleware constructor by removing unnecessary fields

--- backend/Ecommerce/Ecommerce.Api/Api/Middleware/ExceptionMiddleware.cs
+++ backend/Ecommerce/Ecommerce.Api/Api/Middleware/ExceptionMiddleware.cs
@@ -3,26 +3,17 @@
 using Ecommerce.Api.Application.Exceptions;
 
 
-public class ExceptionMiddleware
+public class ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
 {
-    private readonly RequestDelegate _next;
-    private readonly ILogger<ExceptionMiddleware> _logger;
-    
-    public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
-    {
-        _next = next;
-        _logger = logger;
-    }
-    
     public async Task InvokeAsync(HttpContext httpContext)
     {
         try
         {
-            await _next(httpContext);
+            await next(httpContext);
         }
         catch (Exception ex)
         {
-            _logger.LogError(ex, "An unhandled exception occurred.");
+            logger.LogError(ex, "An unhandled exception occurred.");
             await HandleExceptionAsync(httpContext, ex);
         }
     }
